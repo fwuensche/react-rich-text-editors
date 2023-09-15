130d7093141e63a7470539cacd8339886d6e49f4
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-09-15 18:11:41 +0200

    add quill editor

--- src/Quill.js
+++ src/Quill.js
@@ -1,5 +1,40 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const INITIAL_VALUE = `
+<h1>Here Some Title</h1>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+It has survived not only five centuries, but also the leap into electronic typesetting,
+remaining essentially unchanged.</p>
+
+<p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+<h1>And Another Title</h1>
+<p>With a list of topics:</p>
+
+<ul>
+<li>Item 1</li>
+<li>Item 2</li>
+<li>Item 3</li>
+</ul>
+
+<p>And some code:</p>
+<pre>
+const foo = "bar";
+console.log(foo);
+</pre>
+`;
+
 const Quill = () => {
-  return <div>Quill</div>;
+  const [value, setValue] = useState(INITIAL_VALUE);
+
+  console.log(value);
+
+  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
 };
 
 export default Quill;
