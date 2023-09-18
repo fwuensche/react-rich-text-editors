82a09746940101adc91f6cc4ee951ef13db6cb3f
Flavio Wuensche <flavio.wuensche@doctolib.com>
2023-09-18 17:00:31 +0200

    add inline code

--- src/shared.js
+++ src/shared.js
@@ -1,6 +1,6 @@
 export const INITIAL_CONTENT = `
 <h1>Majestic Title</h1>
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+<p>Lorem Ipsum is simply <b>dummy</b> text of the <i>printing and typesetting</i> industry.
 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 It has survived not only five centuries, but also the leap into electronic typesetting,
@@ -17,6 +17,7 @@ and more recently with desktop publishing software like Aldus PageMaker includin
   <li>Performance</li>
   <li>Maintainability</li>
   <li>Documentation</li>
+  <li>Can inline <code>snippets</code> into text nodes</li>
 </ul>
 
 <p>And some sample code:</p>
